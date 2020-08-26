3f38bddfe9e82f9379a4eb04a65ab3abb95725a8
Fernando Robles Rivera <starla.frr@gmail.com>
2020-08-26 14:07:44 -0500

    feat(home): se crea estructura flexible para la vista principal del test

--- src/Modules/Header/index.js
+++ src/Modules/Header/index.js
@@ -5,5 +5,9 @@ import React from "react";
 import styles from "./index.module.sass";
 
 export default function Header() {
-  return <div className={styles.container}>Header</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Header</div>
+    </div>
+  );
 }
