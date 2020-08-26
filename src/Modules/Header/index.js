ad46ea4159e23f6fc3e37856b03481f6746f9bef
Fernando Robles Rivera <starla.frr@gmail.com>
2020-08-26 17:07:29 -0500

    feat(header): avance de maqueta de header

--- src/Modules/Header/index.js
+++ src/Modules/Header/index.js
@@ -1,13 +1,30 @@
 // Librerías
 import React from "react";
 
+// Componentes
+import { ReactComponent as LogoIcon } from "../../Components/Icons/logo-icon.svg";
+
+import Search from "../Search";
+
 // Estilos
 import styles from "./index.module.sass";
 
 export default function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>Header</div>
+      <div className={styles.content}>
+        <div className={styles.logo}>logo</div>
+        <div className={styles.search}>
+          <Search />
+        </div>
+        <div className={styles.profileData}>
+          <div className={styles.user}>
+            <div className={styles.joinPathsUserIcon}></div>
+            <span>Fernando Robles</span>
+          </div>
+          <div className={styles.cart}></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Modules/Search/index.js
+++ src/Modules/Search/index.js
@@ -1,9 +1,17 @@
 // Librerías
 import React from "react";
 
+// Componentes
+import { ReactComponent as SearchIcon } from "../../Components/Icons/search-icon.svg";
+
 // Estilos
 import styles from "./index.module.sass";
 
 export default function Search() {
-  return <div className={styles.container}>Search</div>;
+  return (
+    <div className={styles.container}>
+      <p>search</p>
+      <SearchIcon />
+    </div>
+  );
 }
