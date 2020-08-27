58d6507d42dd87d2ee400cbca1083777226514b2
Fernando Robles Rivera <32597929+starla01@users.noreply.github.com>
2020-08-27 12:09:30 -0500

    feat(home): se agrega componente de banner con dezplazamiento e interaccion con bullets

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,13 +1,18 @@
 // Librerías
-import React from "react";
+import React from 'react';
 
 // Estilos
-import styles from "./index.module.sass";
+import styles from './index.module.sass';
+
+// Modules
+import Banner from '../../Modules/Banner';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.banner}>Banner</div>
+      <div className={styles.banner}>
+        <Banner />
+      </div>
       <div className={styles.content}>Home</div>
     </div>
   );
