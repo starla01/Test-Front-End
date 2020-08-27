f1d4976b53c53f3540f6ce2381b5d5eeecebd3ad
Fernando Robles Rivera <32597929+starla01@users.noreply.github.com>
2020-08-26 23:41:13 -0500

    feat(footer): se inicia maqueta de footer

--- src/Modules/Footer/index.js
+++ src/Modules/Footer/index.js
@@ -1,8 +1,8 @@
 // Librerías
-import React from "react";
+import React from 'react';
 
 // Estilos
-import styles from "./index.module.sass";
+import styles from './index.module.sass';
 
 export default function Footer() {
   return (
@@ -11,7 +11,20 @@ export default function Footer() {
         <div className={styles.top}>content Top</div>
       </div>
       <div className={styles.containerBottom}>
-        <div className={styles.bottom}>content Bottom</div>
+        <div className={styles.bottom}>
+          <div id={styles.addressInfo} className={styles.block}>
+            <h2 className={styles.title}>Localização</h2>
+            <div className={styles.assetLine}></div>
+            <div className={styles.address}>
+              <p>Avenida Andrômeda, 2000. Bloco 6 e 8 </p>
+              <p>Alphavile SP </p>
+              <p>brasil@corebiz.ag </p>
+              <p>+55 11 3090 1039</p>
+            </div>
+          </div>
+          <div id={styles.contactInfo} className={styles.block}></div>
+          <div id={styles.enterpriseInfo} className={styles.block}></div>
+        </div>
       </div>
     </div>
   );
