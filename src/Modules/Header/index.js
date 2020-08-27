7aba175afb224c3795a003bfbe31ca8f95023748
Fernando Robles Rivera <32597929+starla01@users.noreply.github.com>
2020-08-26 23:37:51 -0500

    feat(header): se concluye maqueta de header

--- src/Modules/Header/index.js
+++ src/Modules/Header/index.js
@@ -1,10 +1,13 @@
-// @flow
-
 // Librerías
 import React from 'react';
 
 // Componentes
+
+// Iconos
 import { ReactComponent as LogoIcon } from '../../Components/Icons/logo-icon.svg';
+import { ReactComponent as HeadUserIcon } from '../../Components/Icons/userhead-icon.svg';
+import { ReactComponent as BodyUserIcon } from '../../Components/Icons/userbody-icon.svg';
+import { ReactComponent as CartIcon } from '../../Components/Icons/cart.svg';
 
 import Search from '../Search';
 
@@ -15,16 +18,25 @@ export default function Header() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.logo}>logo</div>
+        <div className={styles.logo}>
+          <LogoIcon className={styles.logo} />
+          <div className={styles.dot}></div>
+        </div>
         <div className={styles.search}>
           <Search />
         </div>
         <div className={styles.profileData}>
           <div className={styles.user}>
-            <div className={styles.joinPathsUserIcon}></div>
-            <span>Fernando Robles</span>
+            <div className={styles.joinPathsUserIcon}>
+              <HeadUserIcon />
+              <BodyUserIcon />
+            </div>
+            <span className={styles.name}>Minha Conta </span>
           </div>
-          <div className={styles.cart}></div>
+          <div className={styles.cart}>
+            <CartIcon />
+            <div className={styles.countItems}>1</div>
+          </div>
         </div>
       </div>
     </div>
