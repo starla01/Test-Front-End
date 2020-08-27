8bdbcbc0e6483ed70e6c731d1a8197b7663df5f8
Fernando Robles Rivera <32597929+starla01@users.noreply.github.com>
2020-08-27 12:15:04 -0500

    feat(home): se agrega componente de banner con dezplazamiento e interaccion con bullets (#8)

--- src/Modules/Banner/index.js
+++ src/Modules/Banner/index.js
@@ -1,9 +1,82 @@
-// Librerías
-import React from 'react';
+// Librerias
+import React, { useState } from 'react';
 
 // Estilos
 import styles from './index.module.sass';
 
+// Iconos
+import { ReactComponent as BackBlack } from '../../Components/Icons/banner.svg';
+
+const data = [
+  {
+    image: '/images/banner.png',
+    title: 'Olá, o que você está buscando?',
+    text: 'Criar ou migrar seu e-commerce?',
+  },
+  {
+    image: '/images/banner.png',
+    title: 'Olá, o que você está buscando?',
+    text: 'Criar ou migrar seu e-commerce?',
+  },
+  {
+    image: '/images/banner.png',
+    title: 'Olá, o que você está buscando?',
+    text: 'Criar ou migrar seu e-commerce?',
+  },
+  {
+    image: '/images/banner.png',
+    title: 'Olá, o que você está buscando?',
+    text: 'Criar ou migrar seu e-commerce?',
+  },
+];
+
 export default function Banner() {
-  return <div className={styles.container}>Banner</div>;
+  const [positionBanner, setPositionBanner] = useState(0);
+  const left = `${positionBanner * -100}%`;
+  const widthBanner = `${data.length * 100}%`;
+
+  function Bullets() {
+    return (
+      <div className={styles.containerBullets}>
+        {data.map((data, key) => {
+          return (
+            <div
+              key={key}
+              className={`${(key === positionBanner && styles.active) || ''} ${styles.bullet}`}
+              onClick={() => setPositionBanner(key)}></div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.containerBanner}>
+      <Bullets />
+      <div
+        className={styles.containerImages}
+        style={{
+          left: left,
+          width: widthBanner,
+        }}>
+        {data.map((banner, key) => {
+          const { image, title, text } = banner;
+          return (
+            <div key={key} className={styles.image}>
+              <div className={styles.imageBanner}>
+                <img src={image} className={styles.assteImage} alt="" />
+              </div>
+              <div className={styles.backBlack}>
+                <div className={styles.containerInfo}>
+                  <div className={styles.title}>{title}</div>
+                  <div className={styles.text}>{text}</div>
+                </div>
+                <BackBlack className={styles.backBanner} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
